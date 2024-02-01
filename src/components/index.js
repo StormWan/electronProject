81d9d16e41e3838b54a48fedc8b14f9470e62173
临江仙 <2607881950@qq.com>
2024-02-01 08:29:37 +0800

    解决报错

--- src/components/index.js
+++ src/components/index.js
@@ -2,15 +2,15 @@
  * @description 加载所有组件
  * @param  {ReturnType<typeofcreateApp>} app 整个应用的实例
  */
-const whiteList = ["SvgIcon"]; //组件白名单
+const whiteList = ["SvgIcon", "Stage"]; //组件白名单
 
 /** 自动加载全局组件 */
 export function loadAllassembly(app) {
   const files = require.context("./", true, /\.vue$/);
   files.keys().forEach((key) => {
     const name = key.replace(/\.\/(.*)\/index\.vue/, "$1"); // 获取组件名称
-    const meter = files(key).default;
     if (!whiteList.includes(name)) {
+      const meter = files(key).default;
       app.component(name, meter);
     }
   });
