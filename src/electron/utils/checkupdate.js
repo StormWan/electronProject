b7581fc829d4fff2fda2314d3b2bba7f83966672
mac <2607881950@qq.com>
2024-03-08 22:47:17 +0800

    增量更新

--- src/electron/utils/checkupdate.js
+++ src/electron/utils/checkupdate.js
@@ -1,20 +1,24 @@
 import { app, ipcMain } from "electron";
 import { autoUpdater } from "electron-updater";
 import { isWindows, isMac } from "@/electron/utils/index";
-// const logger = require("../logger/index");
+import { windowMap } from "../utils/windows-map";
+
 /*
- * -1 检查更新失败 0 正在检查更新 1 检测到新版本，准备下载 2 未检测到新版本 3 下载中 4 下载完成
+ * -1 检查更新失败
+ * 0 正在检查更新
+ * 1 检测到新版本，准备下载
+ * 2 未检测到新版本
+ * 3 下载中
+ * 4 下载完成
  */
 
 // 配置更新服务器 URL
 const updateServerUrl = process.env.VUE_APP_UPDATE_SERVER_URL;
 
 // 负责向渲染进程发送信息
-function sendMessage(type, data) {
-  const mainWin = global.mainWin;
-  console.log("mainWin", mainWin);
-  if (mainWin) return;
-  console.log("mainWin", mainWin);
+function sendMessage({ type, data }) {
+  const mainWin = windowMap.get("mainWin");
+  if (!mainWin) return;
   mainWin.webContents.send("updateMsg", { type, data });
 }
 
@@ -27,46 +31,32 @@ class Update {
     autoUpdater.autoInstallOnAppQuit = false;
     // 当更新发生错误的时候触发。
     autoUpdater.on("error", (err) => {
-      // logger.error("更新出现错误", err);
-      console.log("更新出现错误", err);
       sendMessage({ type: "error", data: err });
     });
     // 当开始检查更新的时候触发
     autoUpdater.on("checking-for-update", (event, arg) => {
-      // logger.info("开始检查更新", arg);
-      console.log("开始检查更新", arg);
-      console.log(this.mainWin);
-      sendMessage({ type: "checking-for-update", data: arg });
+      sendMessage({ type: "checking-for-update", data: "开始检查更新" });
     });
     // 发现可更新数据时
     autoUpdater.on("update-available", (event, arg) => {
-      // logger.info("有更新", arg);
-      console.log("有更新", arg);
-      sendMessage({ type: "update-available", data: arg });
+      sendMessage({ type: "update-available", data: "有更新" });
     });
     // 没有可更新数据时
     autoUpdater.on("update-not-available", (event, arg) => {
-      // logger.info("没有更新", arg);
-      console.log("没有更新", arg);
-      sendMessage({ type: "update-not-available", data: arg });
+      sendMessage({ type: "update-not-available", data: "无更新" });
     });
     // 下载监听
     autoUpdater.on("download-progress", (progress) => {
-      // logger.info("进度", progress);
-      console.log("进度", progress);
       sendMessage({ type: "download-progress", data: progress });
     });
     // 下载完成
     autoUpdater.on("update-downloaded", () => {
-      // logger.info("下载完成 done");
-      console.log("下载完成 done");
-      sendMessage({ type: "update-downloaded", data: "done" });
+      sendMessage({ type: "done", data: "done" });
     });
   }
-  // 执行自动更新检查
+  // 执行更新检查
   checkUpdate() {
     autoUpdater.checkForUpdates().catch((err) => {
-      console.log("网络连接问题", err);
       sendMessage({ type: "checkForUpdates", data: err });
     });
   }
--- src/utils/appEmit.js
+++ src/utils/appEmit.js
@@ -3,12 +3,29 @@ const { ipcRenderer } = require("electron");
 
 export const appIpcEmit = () => {
   ipcRenderer.on("awaken", (event, data) => {
-    console.warn("awaken:", data);
+    console.log("awaken:", data);
     const { queryStringToObject } = require("@/utils/chat/message-input-utils");
-    console.warn(queryStringToObject(data));
+    console.log(queryStringToObject(data));
   });
-  ipcRenderer.on("updateMsg", (event, data) => {
-    console.warn("updateMsg:", data);
+  ipcRenderer.on("updateMsg", (event, { data, type }) => {
+    console.log(type, data);
+    let downloadComplete = false; //下载完成
+    let downloadProgress = 0; //下载进度
+    let hasupdates = false; //是否更新可用
+    if (type === "update-available") {
+      // 有更新 hasupdates true
+    } else if (type === "update-not-available") {
+      // 无更新 hasupdates = false
+    }
+    if (!hasupdates) return;
+    // 下载进度
+    if (type === "download-progress") {
+      const { percent } = data;
+      downloadProgress = parseInt(percent) || 0;
+    } else if (type === "done") {
+      downloadProgress = 100;
+      downloadComplete = true;
+    }
   });
 
   // const options = {
